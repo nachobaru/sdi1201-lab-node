d162aaac97f21002b342fdb76e9e4de15157f41e
nachobaru <ignaciobaruque@gmail.com>
2019-03-31 16:33:00 +0200

    SDI-NodeJS-9.4- Mensajes y alertas

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -15,9 +15,13 @@ module.exports = function(app, swig, gestorBD) {
         }
         gestorBD.insertarUsuario(usuario, function(id) {
             if (id == null){
-                res.send("Error al insertar ");} else {
-               app.redirect("/identificarse");
+                res.send("Error al insertar ");
+                res.redirect("/registrarse?mensaje=Error al registrar usuario")
+            } else {
+                res.send('Usuario Insertado ' + id);
+                res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
             }
+
         });
 
     })
@@ -35,7 +39,10 @@ module.exports = function(app, swig, gestorBD) {
         gestorBD.obtenerUsuarios(criterio, function(usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
-                res.send("No identificado: ");
+                res.redirect("/identificarse" +
+                    "?mensaje=Email o password incorrecto"+
+                    "&tipoMensaje=alert-danger ");
+
             } else {
                 req.session.usuario = usuarios[0].email;
                 res.redirect("/publicaciones");
